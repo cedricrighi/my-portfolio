9b1254d1536d1add06f088ab1d2d8a0caf022e40
cedricrighi <cedric.righi@gmail.com>
2025-05-14 15:52:15 +0200

    fix: update cv and technos fav

--- src/components/TechnosFav.tsx
+++ src/components/TechnosFav.tsx
@@ -2,25 +2,32 @@ import { useTheme } from "../contexts/themeContext";
 import "../styles/TechnosFav.css";
 
 export default function TechnosFav() {
-	const technosFav = ["React", "Vite", "TypeScript", "MySQL"];
-	const { theme } = useTheme();
+  const technosFav = [
+    "React",
+    "Angular",
+    "Vite",
+    "TypeScript",
+    "MySQL",
+    "Express",
+  ];
+  const { theme } = useTheme();
 
-	return (
-		<div
-			className="technos-fav-container"
-			style={{
-				boxShadow: theme
-					? "1px 3px 10px -4px #272727"
-					: "1px 3px 20px -4px rgb(255, 255, 255)",
-			}}
-		>
-			<h4>Technologies préférées</h4>
-			<div className="technos-fav-list-container">
-				{technosFav.map((techno, index) => {
-					const id = index + 1;
-					return <p key={id}>{techno}</p>;
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="technos-fav-container"
+      style={{
+        boxShadow: theme
+          ? "1px 3px 10px -4px #272727"
+          : "1px 3px 20px -4px rgb(255, 255, 255)",
+      }}
+    >
+      <h4>Technologies préférées</h4>
+      <div className="technos-fav-list-container">
+        {technosFav.map((techno, index) => {
+          const id = index + 1;
+          return <p key={id}>{techno}</p>;
+        })}
+      </div>
+    </div>
+  );
 }
